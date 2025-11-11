3d3d6f5dea26c5f34292b0b4d3ca3edd861d0621
dangstudentatecc <dang.nguyenkhoa289@gmail.com>
2025-11-12 02:28:20 +0900

    Delete product_introduce_RM directory

--- product_introduce_RM/js/language_droplist_function.js
+++
@@ -1,8 +0,0 @@
-import { changeLanguage } from './language_switcher.js';
-
-document.querySelectorAll('.lang-option').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const selectedLang = btn.dataset.lang;
-    changeLanguage(selectedLang);
-  });
-});
